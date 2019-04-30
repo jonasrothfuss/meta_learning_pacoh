e986f4feef57b7368a258535c8146d600562080c
jonasrothfuss <jonas.rothfuss@gmail.com>
2019-04-30 17:13:21 +0200

    simulation of sinusoidal classification data

--- src/data_sim.py
+++ src/data_sim.py
@@ -7,12 +7,15 @@ from matplotlib import pyplot as plt
 X_LOW = -5
 X_HIGH = 5
 
+Y_HIGH = 2.5
+Y_LOW = -2.5
+
 
 """ sinusoidal data """
 
 # sinusoid function + gaussian noise
-def _sinusoid(x, amplitude=1.0, period=1.0, x_shift=0.0, y_shift=0.0, noise_std=0.0):
-    f = amplitude * np.sin(period * (x - x_shift)) + y_shift
+def _sinusoid(x, amplitude=1.0, period=1.0, x_shift=0.0, y_shift=0.0, slope=0.0, noise_std=0.0):
+    f = slope*x + amplitude * np.sin(period * (x - x_shift)) + y_shift
     noise = np.random.normal(0, scale=noise_std, size=f.shape)
     return f + noise
 
@@ -22,7 +25,7 @@ def _sample_sinusoid(amp_low=0.2, amp_high=2.0, y_shift_std=0.3, noise_std=0.1):
     y_shift = np.random.normal(scale=y_shift_std)
     return lambda x: _sinusoid(x, amplitude=amplitude, y_shift=y_shift, noise_std=noise_std)
 
-def sample_sinusoid_data(size=1, amp_low=0.2, amp_high=2.0, y_shift_std=0.3, noise_std=0.1):
+def sample_sinusoid_regression_data(size=1, amp_low=0.2, amp_high=2.0, y_shift_std=0.3, noise_std=0.1):
     """ samples a sinusoidal function and then data from the respective function
 
         Args:
@@ -47,8 +50,35 @@ def sample_sinusoid_data(size=1, amp_low=0.2, amp_high=2.0, y_shift_std=0.3, noi
 
     return X, Y
 
+
+def sample_sinusoid_regression_data(size=1, amp_low=0.2, amp_high=2.0, y_shift_std=0.3, noise_std=0.1):
+    """ samples classification data with a sinusoidal separation function
+           Args:
+                 amp_low (float): min amplitude value
+                 amp_high (float): max amplitude value
+                 y_shift_std (float): std of Gaussian from which to sample the y_shift of the sinusoid
+                 noise_std (float): std of the Gaussian observation noise
+
+           Returns:
+               (X, t): X is an ndarray of dimensionality (size, 2) and t an ndarray of dimensionality (size,)
+                       containing {-1, 1} entries
+       """
+
+    if isinstance(size, Number):
+        size = (int(size),)  # convert to tuple
+
+    f = _sample_sinusoid(amp_low=amp_low, amp_high=amp_high, y_shift_std=y_shift_std, noise_std=noise_std)
+    X_1 = np.random.uniform(X_LOW, X_HIGH, size=size + (1,)) # first data dimension
+    Y = np.random.uniform(Y_LOW, Y_HIGH, size=size + (1,)) # second data dimension
+    target = ((f(X_1) > Y ) * 2 - 1).flatten() # convert {0,1} to {-1, 1} data
+    X = np.concatenate([X_1, Y], axis=-1)
+    assert np.all(np.logical_or(target == 1.0, target -1.0))
+    return X, target
+
+
+
 if __name__ == "__main__":
-    X, Y = sample_sinusoid_data(10)
+    X, Y = sample_sinusoid_regression_data(100)
     print(X.shape)
-    plt.scatter(X, Y)
+    plt.scatter(X[:,0], X[:,1], c=Y)
     plt.show()
