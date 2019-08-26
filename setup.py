bbcc12db3d0e011c70f5dd694c59bf87b67c81bf
Jonas Rothfuss <jonas.rothfuss@gmx.de>
2019-08-26 15:17:08 +0200

    corrections in setup.py

--- setup.py
+++ setup.py
@@ -18,9 +18,4 @@ setuptools.setup(
         'numpy'
         'gpytorch'
     ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
 )
