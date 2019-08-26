ac5a19903f41699ffcbee88deca10ee083dcf876
Jonas Rothfuss <jonas.rothfuss@gmx.de>
2019-08-26 15:14:39 +0200

    travis.yml and setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="meta-gp",
+    version="0.0.1",
+    author="Jonas Rothfuss",
+    author_email="jonas.rothfuss@gmail.com",
+    description="Meta-Learning Gausssian Proccess Priors",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir='src',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'torch',
+        'numpy'
+        'gpytorch'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
