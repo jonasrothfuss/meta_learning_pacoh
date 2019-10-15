1cac24ba3bbb90c67c64e5b6854cf1f1908cffcf
Jonas Rothfuss <jonas.rothfuss@gmx.de>
2019-10-15 11:29:38 +0200

    data sim from GP prior

--- experiments/data_sim.py
+++ experiments/data_sim.py
@@ -281,43 +281,46 @@ class SinusoidNonstationaryDataset(MetaDataset):
         mean = lambda x: slope * x
         return lambda x: mean(x) + np.sin(freq(x) * x) + self.random_state.normal(loc=0, scale=self.noise_std, size=x.shape)
 
-# """ sinusoidal data """
-#
-# # sinusoid function + gaussian noise
-# def _sinusoid(x, amplitude=1.0, period=1.0, x_shift=0.0, y_shift=0.0, slope=0.0, noise_std=0.0):
-#     f = slope*x + amplitude * np.sin(period * (x - x_shift)) + y_shift
-#     noise = np.random.normal(0, scale=noise_std, size=f.shape)
-#     return f + noise
-#
-#
-# def sample_sinusoid_classification_data(size=1, amp_low=0.5, amp_high=1.5, y_shift_mean=5.0, y_shift_std=0.3,
-#                                         slope_mean=0.2, slope_std=0.05, noise_std=0.1):
-#     """ samples classification data with a sinusoidal separation function
-#            Args:
-#                 amp_low (float): min amplitude value
-#                 amp_high (float): max amplitude value
-#                 y_shift_mean (float): mean of Gaussian from which to sample the y_shift of the sinusoid
-#                 y_shift_std (float): std of Gaussian from which to sample the y_shift of the sinusoid
-#                 slope_mean (float: mean of Gaussian from which to sample the linear slope
-#                 slope_std (float): std of Gaussian from which to sample the linear slope
-#                 noise_std (float): std of the Gaussian observation noise
-#
-#            Returns:
-#                (X, t): X is an ndarray of dimensionality (size, 2) and t an ndarray of dimensionality (size,)
-#                        containing {-1, 1} entries
-#        """
-#
-#     if isinstance(size, Number):
-#         size = (int(size),)  # convert to tuple
-#
-#     f = _sample_sinusoid(amp_low=amp_low, amp_high=amp_high, y_shift_mean=y_shift_mean, y_shift_std=y_shift_std,
-#                          slope_mean=slope_mean, slope_std=slope_std, noise_std=noise_std)
-#     X_1 = np.random.uniform(X_LOW, X_HIGH, size=size + (1,)) # first data dimension
-#     Y = np.random.uniform(y_shift_mean + Y_LOW, y_shift_mean + Y_HIGH, size=size + (1,)) # second data dimension
-#     target = np.sign(Y - f(X_1)).flatten()
-#     X = np.concatenate([X_1, Y], axis=-1)
-#     assert np.all(np.logical_or(target == 1.0, target -1.0))
-#     return X, target
+class GPFunctionsDataset(MetaDataset):
+
+    def __init__(self, noise_std=0.0, lengthscale=1.0, mean=0.0, x_low=-5, x_high=5, random_state=None):
+        self.noise_std, self.lengthscale, self.mean = noise_std, lengthscale, mean
+        self.x_low, self.x_high = x_low, x_high
+        super().__init__(random_state)
+
+    def generate_meta_test_data(self, n_tasks, n_samples_context, n_samples_test):
+        assert n_samples_test > 0
+        meta_test_tuples = []
+        for i in range(n_tasks):
+            X = self.random_state.uniform(self.x_low, self.x_high, size=(n_samples_context + n_samples_test, 1))
+            Y = self._gp_fun_from_prior(X)
+            meta_test_tuples.append(
+                (X[n_samples_context:], Y[n_samples_context:], X[:n_samples_context], Y[:n_samples_context]))
+
+        return meta_test_tuples
+
+    def generate_meta_train_data(self, n_tasks, n_samples):
+        meta_train_tuples = []
+        for i in range(n_tasks):
+            X = self.random_state.uniform(self.x_low, self.x_high, size=(n_samples, 1))
+            Y = self._gp_fun_from_prior(X)
+            meta_train_tuples.append((X, Y))
+        return meta_train_tuples
+
+    def _gp_fun_from_prior(self, X):
+        assert X.ndim == 2
+
+        n = X.shape[0]
+
+        def kernel(a, b, lengthscale):
+            sqdist = np.sum(a ** 2, 1).reshape(-1, 1) + np.sum(b ** 2, 1) - 2 * np.dot(a, b.T)
+            return np.exp(-.5 * (1 / lengthscale) * sqdist)
+
+        K_ss = kernel(X, X, self.lengthscale)
+        L = np.linalg.cholesky(K_ss + 1e-8 * np.eye(n))
+        f = self.mean + np.dot(L, self.random_state.normal(size=(n, 1)))
+        y = f + np.random.normal(scale=self.noise_std, size=f.shape)
+        return y
 
 
 if __name__ == "__main__":
